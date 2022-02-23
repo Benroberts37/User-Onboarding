5717af99a0e1bd24639704f70567c960bfe899af
Benroberts37 <benrobsemail@gmail.com>
2022-02-22 16:28:50 -0800

    Almost finished, need to fix a few things

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState} from 'react'
+import './App.css';
+
+import * as yup from 'yup';
+import Form from './Components/Form'
+import schema from './Validation/formSchema'
+
+const initialFormlValues = {
+  username: '',
+  password: '',
+  email: '',
+  tos: false
+}
+
+const initialFormErrors = {
+  username: '',
+  password: '',
+  email: '',
+  tos: false
+};
+
+function App() {
+  const [formValues, setFormValues] = useState(initialFormlValues);
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+
+  const handleSubmit = () => {
+
+  };
+
+  const validate = (name, value) => {
+    yup.reach(schema, name)
+      .validate(value)
+      .then(() => setFormErrors({...formErrors, [name]: ''}))
+      .catch(err => setFormErrors({...formErrors, [name]: err.errors[0]}))
+  }
+
+  const handleChange = (name, value) => {
+    validate(name, value);
+    setFormValues({...formValues, [name]:value})
+  }
+  return (
+    <div className="App">
+        <Form values ={formValues} change={handleChange} errors={formErrors} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Validation/formSchema.js
@@ -0,0 +1,23 @@
+import * as yup from 'yup';
+import { string } from 'yup';
+
+const formSchema = yup.object().shape({
+    username:yup    
+        .string()
+        .trime()
+        .required('Username is required')
+        .min(3, 'Username must be 3 characters long'),
+    email: yup  
+        .string()
+        .email('Must be a valid email address')
+        .required('email is required'),
+    password: yup
+        .string()
+        .required('Password is required')
+        .min(6, 'Password must be at least 6 characters'),
+    tos: yup    
+        .boolean()
+        .oneOf([true], 'Must accept terms and conditions')
+})
+
+export default formSchema
